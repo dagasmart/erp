0325b8a845b7738d208e6563d4fbd2d5208cf5b6
raoxikui <raoxikui@qq.com>
2026-01-23 17:26:17 +0800

    优化

--- src/ErpServiceProvider.php
+++ src/ErpServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace DagaSmart\Erp;
 
+use Exception;
+use DagaSmart\BizAdmin\Renderers\Form;
 use DagaSmart\BizAdmin\Renderers\TextControl;
 use DagaSmart\BizAdmin\Extend\ServiceProvider;
 
@@ -285,13 +287,29 @@ class ErpServiceProvider extends ServiceProvider
     ];
 
 
-	public function settingForm()
-	{
-	    return $this->baseSettingForm()->body([
-            TextControl::make()->name('value')->label('Value')->required(true),
-	    ]);
-	}
+    /**
+     * @return void
+     * @throws Exception
+     */
+    public function register(): void
+    {
+        parent::register();
 
+        /**加载路由**/
+        parent::registerRoutes(__DIR__.'/Http/routes.php');
+        /**加载语言包**/
+        if ($lang = parent::getLangPath()) {
+            $this->loadTranslationsFrom($lang, $this->getCode());
+        }
+
+    }
+
+    public function settingForm(): ?Form
+    {
+        return $this->baseSettingForm()->body([
+            TextControl::make()->name('value')->label('Value')->required(),
+        ]);
+    }
     /**
      * 序列号生成器
      * @param string $prefix
